8d54897da9bb17902cccc2b0449a634a5de33ffc
Ryan Lee <ryanlee@zepheira.com>
2011-05-12 01:44:46 -0700

    removing copied segments of test runner

--- scripted/lib/coverage-runner.js
+++ scripted/lib/coverage-runner.js
@@ -1,18 +1,9 @@
 /**
  * @fileOverview Initiates coverage reporting.
  */
-var args = arguments;
+
 (function(){
-    load('lib/setup.js');
+    load('lib/coverage-setup.js');
     console.log("Starting coverage reporting...");
-    // @@@ Passing args to the URL seems broken.  Delete the
-    // following when it works the way it should.  See pre.js.
-    if (Envjs.tmpdir[Envjs.tmpdir.length - 1] !== '/') {
-        Envjs.tmpdir += '/';
-    }
-    Envjs.deleteFile(Envjs.uri(Envjs.tmpdir+"qunitargs"));
-    Envjs.writeToFile("Modules.original = '" + args.join('&') + "';", Envjs.uri(Envjs.tmpdir+"qunitargs"));
-    window.location = "http://127.0.0.1:9876/build/instrument/src/jscoverage.html?w=../tests/index.html";
-    // Use this instead when it works.
-    // + "?" + args.join('&');
+    window.location = "http://127.0.0.1:9876/build/instrument/tests/index.html";
 })();
