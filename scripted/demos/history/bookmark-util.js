851fc3d46e2aa7f0c66c31562c9881395f799fd1
Ryan Lee <ryanlee@zepheira.com>
2011-06-03 14:46:42 -0700

    adding history poc demo

---
+++ scripted/demos/history/bookmark-util.js
@@ -0,0 +1,39 @@
+Exhibit.Bookmark = {
+    _id: "exhibit-bookmark-display"
+};
+
+Exhibit.Bookmark.generateBookmarkHash = function(state) {
+    if (typeof state.data.state === "undefined") {
+        return "";
+    }
+    return Base64.encode(JSON.stringify(state));
+};
+
+Exhibit.Bookmark.interpretBookmarkHash = function(hash) {
+    return JSON.parse(Base64.decode(hash));
+};
+
+Exhibit.Bookmark.generateBookmark = function() {
+    var hash = Exhibit.Bookmark.generateBookmarkHash(History.getState());
+    return document.location.href + ((hash === "") ? "": "#" + hash);
+};
+
+Exhibit.Bookmark.implementBookmark = function(state) {
+    History.replaceState(state.data, state.title, state.url);
+};
+
+Exhibit.Bookmark.showBookmark = function() {
+    $('#'+Exhibit.Bookmark._id+' input').val(Exhibit.Bookmark.generateBookmark()).focus(function(){this.select()});
+    $('#'+Exhibit.Bookmark._id).toggle();
+};
+
+Exhibit.Bookmark.init = function() {
+    $('#exhibit-bookmark').live('click', Exhibit.Bookmark.showBookmark);
+    if (document.location.hash.length > 0) {
+        Exhibit.Bookmark.implementBookmark(Exhibit.Bookmark.interpretBookmarkHash(document.location.hash.substr(1)));
+    }
+};
+
+$(document).ready(function() {
+    Exhibit.Bookmark.init();
+});
