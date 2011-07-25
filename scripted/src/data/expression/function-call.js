d62c6ca239680df766a13f1036889066bce9779d
Ryan Lee <ryanlee@zepheira.com>
2011-07-25 16:35:43 -0700

    expression language pieces

---
+++ scripted/src/data/expression/function-call.js
@@ -0,0 +1,42 @@
+/**
+ * @fileOverview
+ * @author David Huynh
+ * @author <a href="mailto:ryanlee@zepheira.com">Ryan Lee</a>
+ */
+
+/**
+ * @class
+ * @constructor
+ * @param {String} name
+ * @param {Array} args
+ */
+Exhibit.Expression._FunctionCall = function(name, args) {
+    this._name = name;
+    this._args = args;
+};
+
+/**
+ * @param {Object} roots
+ * @param {Object} rootValueTypes
+ * @param {String} defaultRootName
+ * @param {Exhibit.Database} database
+ * @returns {Exhibit.Expression._Collection}
+ * @throws Error
+ */
+Exhibit.Expression._FunctionCall.prototype.evaluate = function(
+    roots, 
+    rootValueTypes, 
+    defaultRootName, 
+    database
+) {
+    var args = [], i;
+    for (i = 0; i < this._args.length; i++) {
+        args.push(this._args[i].evaluate(roots, rootValueTypes, defaultRootName, database));
+    }
+    
+    if (Exhibit.Functions.hasOwnProperty(this._name)) {
+        return Exhibit.Functions[this._name].f(args);
+    } else {
+        throw new Error("No such function named " + this._name);
+    }
+};
