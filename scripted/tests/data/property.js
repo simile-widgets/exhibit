78a8b0af3b61241908b07fe2081913c49425d137
Ryan Lee <ryanlee@zepheira.com>
2011-08-06 01:34:08 -0700

    placeholders

---
+++ scripted/tests/data/property.js
@@ -0,0 +1,57 @@
+module("Exhibit.Database._Property");
+
+test("prototype", function() {
+    //expect();
+});
+
+test("getID", function() {
+    //expect();
+});
+
+test("getURI", function() {
+    //expect();
+});
+
+test("getValueType", function() {
+    //expect();
+});
+
+test("getLabel", function() {
+    //expect();
+});
+
+test("getPluralLabel", function() {
+    //expect();
+});
+
+test("getReverseLabel", function() {
+    //expect();
+});
+
+test("getReversePluralLabel", function() {
+    //expect();
+});
+
+test("getGroupingLabel", function() {
+    //expect();
+});
+
+test("getGroupingPluralLabel", function() {
+    //expect();
+});
+
+test("getOrigin", function() {
+    //expect();
+});
+
+test("getRangeIndex", function() {
+    //expect();
+});
+
+test("_onNewData", function() {
+    //expect();
+});
+
+test("_buildRangeIndex", function() {
+    //expect();
+});
---
+++ scripted/tests/data/range-index.js
@@ -0,0 +1,33 @@
+module("Exhibit.Database._RangeIndex");
+
+test("prototype", function() {
+    //expect();
+});
+
+test("getCount", function() {
+    //expect();
+});
+
+test("getMin", function() {
+    //expect();
+});
+
+test("getMax", function() {
+    //expect();
+});
+
+test("getRange", function() {
+    //expect();
+});
+
+test("getSubjectsInRange", function() {
+    //expect();
+});
+
+test("countRange", function() {
+    //expect();
+});
+
+test("_indexOf", function() {
+    //expect();
+});
