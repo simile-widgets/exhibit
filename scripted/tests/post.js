bee9ec1af8c393c909eafe4d4682afd68b68b047
Ryan Lee <ryanlee@zepheira.com>
2011-08-08 21:08:24 -0700

    with post relying on exhibit signals, no need to use lab for script ordering here

--- scripted/tests/post.js
+++ scripted/tests/post.js
@@ -3,12 +3,16 @@
  * Modules.args, or if none, run all.
  */
 
-if (Modules.args.length === 0) {
-    for (var mod in Modules.tests) {
-        Modules.loadScript(Modules.tests[mod]);
-    }
-} else {
-    for (var i = 0; i < Modules.args.length; i++) {
-        Modules.loadScript(Modules.tests[Modules.args[i]]);
-    }
-}
+$(document).ready(function() {
+    $(document).one("scriptsLoaded.exhibit", function(event) {
+        if (Modules.args.length === 0) {
+            for (var mod in Modules.tests) {
+                Modules.loadScript(Modules.tests[mod]);
+            }
+        } else {
+            for (var i = 0; i < Modules.args.length; i++) {
+                Modules.loadScript(Modules.tests[Modules.args[i]]);
+            }
+        }
+    });
+});
