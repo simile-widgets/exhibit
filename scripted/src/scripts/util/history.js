ddf154a22a84caf9a0cafcb38d98fd4eb619f4b9
Ryan Lee <ryanlee@zepheira.com>
2011-08-23 21:54:54 -0700

    running bookmark from within, fleshing out the role of various methods and making the behavior more solid and deterministic

--- scripted/src/scripts/util/history.js
+++ scripted/src/scripts/util/history.js
@@ -13,14 +13,9 @@
  */
 Exhibit.History = {
     /**
-     *
+     * Whether the history module is available.
      */
     enabled: false,
-
-    /**
-     * @private
-     */
-    _registeredComponents: [],
 
     /**
      * @private
@@ -30,45 +25,82 @@ Exhibit.History = {
     /**
      * @private
      */
-    _originalTitle: ''
+    _originalTitle: "",
+
+    /**
+     * @private
+     */
+    _originalLocation: ""
 };
 
 /**
- * 
- *
  * @depends History.js
  */
 Exhibit.History.init = function() {
+    var state, types, i, j, keys, component;
+
     if (typeof History !== "undefined" && History.enabled) {
         Exhibit.History.enabled = true;
         Exhibit.History._originalTitle = document.title;
+        Exhibit.History._originalLocation = Exhibit.Persistence.getURLWithoutQueryAndHash();
 
-        Exhibit.History.stateListener();
-        $(window).bind('statechange', Exhibit.History.stateListener);
-        // initial state without bookmarking lacks a title, replaceState
-        // should run if no bookmark was executed first to set the title
-        // appropriately
-        if (!Exhibit.Bookmark.run) {
-            Exhibit.History.pushState(null, Exhibit.History._originalTitle, null);
+        $(window).bind("statechange", Exhibit.History.stateListener);
+        if (Exhibit.Bookmark.runBookmark()) {
+            Exhibit.Bookmark.implementBookmark(Exhibit.Bookmark.state);
+        } else {
+            state = Exhibit.History.getState();
+            if (typeof state.data.components === "undefined") {
+                state.data.components = {};
+                state.data.state = Exhibit.History._state;
+                types = [ "facet", "view", "viewPanel" ];
+                for (i = 0; i < types.length; i++) {
+                    keys = Exhibit.Registry.getKeys(types[i]);
+                    for (j = 0; j < keys.length; j++) {
+                        component = Exhibit.Registry.get(types[i], keys[j]);
+                        if (typeof component.exportState === "function") {
+                            state.data.components[keys[j]] = {};
+                            state.data.components[keys[j]].type = types[i];
+                            state.data.components[keys[j]].state = component.exportState();
+                        }
+                    }
+                }
+                Exhibit.History.replaceState(state.data);
+            }
+            Exhibit.History.stateListener();
         }
     }
 };
 
 /**
- *
+ * @param {jQuery.Event} evt
  */
-Exhibit.History.stateListener = function() {
-};
+Exhibit.History.stateListener = function(evt) {
+    var fullState, components, key, id, componentState, component;
 
-/**
- * Register any thing on the page that maintains state in response to user
- * interaction.  Registered components are polled after each interaction
- * to gather history tracking information.  Each component must implement
- * the method getState that returns identifying information and the current
- * state, enough to replicate the current state to a different user.
- *
- */
-Exhibit.History.register = function() {
+    fullState = Exhibit.History.getState();
+
+    if (fullState.data.lengthy) {
+        $(document).trigger("busyDialogStart.exhibit");
+    }
+
+    components = fullState.data.components;
+    for (key in components) {
+        console.log(key);
+        if (components.hasOwnProperty(key)) {
+            componentState = components[key].state;
+            component = Exhibit.Registry.get(components[key].type, key);
+            if (component !== null &&
+                typeof component.importState === "function") {
+                console.log(componentState);
+                component.importState(componentState);
+            }
+        }
+    }
+    Exhibit.History._state = fullState.data.state || 0;
+
+    if (fullState.data.lengthy) {
+        $(document).trigger("busyDialogEnd.exhibit");
+    }
 };
 
 /**
@@ -85,16 +117,53 @@ Exhibit.History.getState = function() {
     }
 };
 
+Exhibit.History.pushComponentState = function(component, registry, data, subtitle, lengthy) {
+    var state = Exhibit.History.getState();
+
+    if (typeof state === "undefined" || state === null) {
+        state = { "data": { "components": {} } };
+    }
+
+    if (!state.hasOwnProperty("data")) {
+        state.data = { "components": {} };
+    }
+    if (!state.data.hasOwnProperty("components")) {
+        state.data.components = {};
+    }
+
+    state.data.lengthy = lengthy;
+    state.data.components[component.getID()] = {
+        "type": registry,
+        "state": data
+    }
+
+    Exhibit.History.pushState(state.data, subtitle);
+};
+
 /**
  * Passes through to History.js History.pushState function.
  * 
  * @static
- * @param data {Object}
- * @param title {String}
- * @param url {String}
+ * @param {Object} data
+ * @param {String} subtitle
  */
-Exhibit.History.pushState = function(data, title, url) {
+Exhibit.History.pushState = function(data, subtitle) {
+    var title, url;
+
     if (Exhibit.History.enabled) {
+        Exhibit.History._state++;
+        data.state = Exhibit.History._state;
+
+        title = Exhibit.History._originalTitle;
+
+        if (typeof subtitle !== "undefined" &&
+            subtitle !== null &&
+            subtitle !== "") {
+            title += " {" + subtitle + "}";
+        }
+        
+        url = Exhibit.History._originalLocation;
+        
         History.pushState(data, title, url);
     }
 };
@@ -103,12 +172,25 @@ Exhibit.History.pushState = function(data, title, url) {
  * Passes through to History.js History.replaceState function.
  * 
  * @static
- * @param data {Object}
- * @param title {String}
- * @param url {String}
+ * @param {Object} data
+ * @param {String} subtitle
  */
-Exhibit.History.replaceState = function(data, title, url) {
+Exhibit.History.replaceState = function(data, subtitle, url) {
+    var title;
+
     if (Exhibit.History.enabled) {
+        title = Exhibit.History._originalTitle;
+
+        if (typeof subtitle !== "undefined" &&
+            subtitle !== null &&
+            subtitle !== "") {
+            title += " {" + subtitle + "}";
+        }
+
+        if (typeof url === "undefined" || url === null) {
+            url = Exhibit.History._originalLocation;
+        }
+        
         History.replaceState(data, title, url);
     }
 };
