572fb4e883df882297d374568fad925a7dc5e0dc
karger@mit.edu <karger@mit.edu>
2013-10-14 02:19:38 -0400

    typo in locale string

--- scripted/src/locales/en/locale.js
+++ scripted/src/locales/en/locale.js
@@ -54,7 +54,7 @@ Exhibit.Localization.importLocale("en", {
     "%export.smwExporterLabel": "Semantic wikitext",
     "%export.exhibitJsonExporterLabel": "Exhibit JSON",
     "%export.tsvExporterLabel": "Tab Separated Values",
-    "%export.HTMLTableExporterLabel": "HTML Table",
+    "%export.htmlTableExporterLabel": "HTML Table",
     "%export.htmlExporterLabel": "Generated HTML of this view",
     "%export.bibtexExporterLabel": "BibTeX",
 
