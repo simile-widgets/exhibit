25e33369869c627c05aa8c2d71beb2c41c4407c6
karger@mit.edu <karger@mit.edu>
2013-10-22 00:20:19 -0400

    Revised large-data demo to use more facets.

--- scripted/demos/big/makebig.pl
+++ scripted/demos/big/makebig.pl
@@ -1,9 +1,18 @@
 #!/usr/bin/perl
 $count = @ARGV[0];
-print '{ "items": [',"\n";
+print <<END;
+{ "properties": {"mod3" : {"valueType" : "number"},
+                 "mod137" : {"valueType" : "number"}
+                },
+  "items": [
+END
 for ($i=0; $i<$count; $i++) {
     print '{"label": "item', $i, '", "even": ',
     ($i % 2) ? '"no"' : '"yes"',
+    ', "mod3": "',$i%3,'"',
+    ', "mod4": "',$i%4,'"',
+    ', "mod37": "',$i%37,'"',
+    ', "mod137": "',$i%137,'"',
     '},', "\n";
 }
 print '{"label": "last", "even": "maybe"}';
