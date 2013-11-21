8f2aeaf52a8995cb82f1915fb0f1757197e8d053
Tochukwu Okoro <tcokoro@gmail.com>
2013-11-21 00:00:34 -0500

    created EnumeratedFacet class and removed methods form cloud and list facet class

--- scripted/src/scripts/ui/facets/cloud-facet.js
+++ scripted/src/scripts/ui/facets/cloud-facet.js
@@ -5,6 +5,53 @@
  * @author <a href="mailto:axel@pike.org">Axel Hecht</a>
  */
 
+// Static method stuff
+/*
+Exhibit.CloudFacet.create = function(containerElmt, uiContext, jsonParams) {
+}
+
+Exhibit.CloudFacet.createFromDOM = function(containerElmt, uiContext) {
+  var settings = //do something to get settings from dom
+  return Exhibit.CloudFacet.create(.., settings);
+}
+
+// Constructor
+
+Exhibit.EnumeratedFacet = function(conteinterEl, uiCon) {
+  A
+}
+
+
+Exhibit.CloudFacet = function(conteinterEl, uiCon) {
+  Exhibit.EnumeratedFacet.call(this, containerEl, uiCon);
+  C
+
+}
+
+
+Exhibit.ListFacet = function(conteinterEl, uiCon) {
+  A
+  B
+
+}
+
+function buildSettingsFromDOM = function(spec, elem) {
+    ret = {}
+    for key in spec:
+      if key.defaultValue:
+        ret[key] = key.defaultValue
+      if elem.hasAttr(key):
+        str = elem.attr(key)
+         if key.hasType and int:
+            str = parseInt(str)
+
+
+
+    return ret
+}
+
+*/
+
 /**
  * @constructor
  * @class
@@ -25,6 +72,8 @@ Exhibit.CloudFacet = function(containerElmt, uiContext) {
     this._dom = null;
 };
 
+Exhibit.CloudFacet.prototype =  new Exhibit.EnumeratedFacet();
+
 /**
  * @constant
  */
@@ -35,24 +84,54 @@ Exhibit.CloudFacet._settingSpecs = {
 };
 
 /**
- * @param {Object} configuration
+ * @param {Element} configElmt
  * @param {Element} containerElmt
  * @param {Exhibit.UIContext} uiContext
+ * @param {Object} settingsFromDOM
  * @returns {Exhibit.CloudFacet}
  */
-Exhibit.CloudFacet.create = function(configuration, containerElmt, uiContext) {
-    var facet, thisUIContext;
-    thisUIContext = Exhibit.UIContext.create(configuration, uiContext);
-    facet = new Exhibit.CloudFacet(containerElmt, thisUIContext);
-    
+Exhibit.CloudFacet.create = function(configElmt, containerElmt, uiContext, settingsFromDOM) {
+    var facet, thisUIContext, configuration;
+
+    configuration = Exhibit.getConfigurationFromDOM(configElmt);
+    thisUIContext = Exhibit.UIContext.createFromDOM(configElmt, uiContext); 
+    facet = new Exhibit.CloudFacet(
+        (typeof containerElmt !== "undefined" && containerElmt !== null) ?
+            containerElmt :
+            configElmt, 
+        thisUIContext
+    );
+
+    Exhibit.CloudFacet.includeSettingsFromDOM(facet, settingsFromDOM);
     Exhibit.CloudFacet._configure(facet, configuration);
-    
     facet._initializeUI();
     thisUIContext.getCollection().addFacet(facet);
     facet.register();
-    
+
     return facet;
 };
+
+/**
+ * @param {Exhibit.CloudFacet} facet
+ * @param {Object} settingsFromDOM 
+ */
+Exhibit.CloudFacet.includeSettingsFromDOM = function(configElmt, facet, settingsFromDOM){
+    if (settingsFromDOM["expression"] !== "undefined" && settingsFromDOM["expressionString"] !== "undefined") {
+        facet.setExpression(Exhibit.ExpressionParser.parse(settingsFromDOM["expressionString"]));
+        facet.setExpressionString(settingsFromDOM["expressionString"]);
+    }
+
+    if (settingsFromDOM["valueSet"] !== "undefined") {
+        for (i = 0; i < settingsFromDOM["valueSet"].length; i++){
+            facet._valueSet.add(settingsFromDOM["valueSet"][i]);
+        }
+    }
+
+    if (settingsFromDOM["selectMissing"] !== "undefined") {
+        facet._selectMissing = (settingsFromDOM["selectMissing"] === "true");
+    }
+    Exhibit.SettingsUtilities.collectSettingsFromDOM(configElmt, facet.getSettingSpecs(), facet._settings);
+}
 
 /**
  * @param {Element} configElmt
@@ -61,47 +140,50 @@ Exhibit.CloudFacet.create = function(configuration, containerElmt, uiContext) {
  * @returns {Exhibit.CloudFacet}
  */
 Exhibit.CloudFacet.createFromDOM = function(configElmt, containerElmt, uiContext) {
-    var configuration, thisUIContext, facet, expressionString, selection, selectMissing, i;
-    configuration = Exhibit.getConfigurationFromDOM(configElmt);
-    thisUIContext = Exhibit.UIContext.createFromDOM(configElmt, uiContext);
-    facet = new Exhibit.CloudFacet(
-        (typeof containerElmt !== "undefined" && containerElmt !== null) ?
-            containerElmt :
-            configElmt, 
-        thisUIContext
-    );
-    
-    Exhibit.SettingsUtilities.collectSettingsFromDOM(configElmt, facet.getSettingSpecs(), facet._settings);
-    
+    var settingsFromDOM = Exhibit.CloudFacet.buildSettingsFromDOM(configElmt);
+    var facet = Exhibit.CloudFacet.create(configElmt, containerElmt, uiContext, settingsFromDOM);
+    return facet;
+};
+
+/**
+ * @param {Element} configElmt
+ * @returns {Object} settingsFromDOM
+ */
+Exhibit.CloudFacet.buildSettingsFromDOM = function(configElmt) {
+    var settingsFromDOM = {
+        "expression": "undefined",
+        "expressionString" : "undefined",
+        "valueSet" : "undefined",
+        "selectMissing" : "undefined"
+    };
+
     try {
         expressionString = Exhibit.getAttribute(configElmt, "expression");
-        if (typeof expressionString !== "undefined" && expressionString !== null && expressionString.length > 0) {
-            facet.setExpression(Exhibit.ExpressionParser.parse(expressionString));
-            facet.setExpressionString(expressionString);
+        if (typeof expressionString !== "undefined" && expressionString !== null && expressionString.length > 0){
+            settingsFromDOM["expression"] = Exhibit.ExpressionParser.parse(expressionString);
+            settingsFromDOM["expressionString"] = expressionString
         }
 
         selection = Exhibit.getAttribute(configElmt, "selection", ";");
         if (typeof selection !== "undefined" && selection !== null && selection.length > 0) {
+            settingsFromDOM["valueSet"] = [];
             for (i = 0; i < selection.length; i++) {
-                facet._valueSet.add(selection[i]);
+                settingsFromDOM.push(selection[i]);
             }
         }
-        
+
         selectMissing = Exhibit.getAttribute(configElmt, "selectMissing");
         if (typeof selectMissing !== "undefined" && selectMissing !== null && selectMissing.length > 0) {
-            facet._selectMissing = (selectMissing === "true");
+            settingsFromDOM["selectMissing"] = (selectMissing === "true");
         }
+
     } catch (e) {
-        Exhibit.Debug.exception(e, Exhibit._("%facets.error.configuration", "CloudFacet"));
+         Exhibit.Debug.exception(e, Exhibit._("%facets.error.configuration", "CloudFacet"));
     }
-    Exhibit.CloudFacet._configure(facet, configuration);
 
-    facet._initializeUI();
-    thisUIContext.getCollection().addFacet(facet);
-    facet.register();
+    return settingsFromDOM;
+}
 
-    return facet;
-};
 
 /**
  * @param {Exhibit.CloudFacet} facet
@@ -137,60 +219,6 @@ Exhibit.CloudFacet.prototype._dispose = function() {
     this._valueType = null;
     this._missingItems = null;
     this._orderMap = null;
-};
-
-/**
- * @returns {Boolean}
- */
-Exhibit.CloudFacet.prototype.hasRestrictions = function() {
-    return this._valueSet.size() > 0 || this._selectMissing;
-};
-
-/**
- *
- */
-Exhibit.CloudFacet.prototype.clearAllRestrictions = function() {
-    Exhibit.jQuery(this.getContainer()).trigger("onBeforeFacetReset.exhibit");
-    this._valueSet = new Exhibit.Set();
-    this._selectMissing = false;
-    this._notifyCollection();
-};
-
-/**
- * @param {Array} restrictions
- */
-Exhibit.CloudFacet.prototype.applyRestrictions = function(restrictions) {
-    var i;
-    this._valueSet = new Exhibit.Set();
-    for (i = 0; i < restrictions.selection.length; i++) {
-        this._valueSet.add(restrictions.selection[i]);
-    }
-    this._selectMissing = restrictions.selectMissing;
-    
-    this._notifyCollection();
-};
-
-/**
- * @param {String} value
- * @param {Boolean} selected
- */
-Exhibit.CloudFacet.prototype.setSelection = function(value, selected) {
-    if (selected) {
-        this._valueSet.add(value);
-    } else {
-        this._valueSet.remove(value);
-    }
-    this._notifyCollection();
-};
-
-/**
- * @param {Boolean} selected
- */
-Exhibit.CloudFacet.prototype.setSelectMissing = function(selected) {
-    if (selected !== this._selectMissing) {
-        this._selectMissing = selected;
-        this._notifyCollection();
-    }
 };
 
 /**
@@ -256,7 +284,7 @@ Exhibit.CloudFacet.prototype.update = function(items) {
  * @returns {Array}
  */
 Exhibit.CloudFacet.prototype._computeFacet = function(items) {
-    var database, entries, valueType, self, path, facetValueResult, itemSubcollection, value, itemA, count, i, item, sortValueFunction, orderMap, sortFunction, sortDirectionFunction;
+    var database, entries, valueType, self, path, facetValueResult, itemSubcollection, value, itemA, count, i, item
     database = this.getUIContext().getDatabase();
     entries = [];
     valueType = "text";
@@ -305,48 +333,8 @@ Exhibit.CloudFacet.prototype._computeFacet = function(items) {
             entry.actionLabel = entry.selectionLabel = labeler(entry.value);
             entry.selected = selection.contains(entry.value);
         }
-        
-        sortValueFunction = function(a, b) { return a.selectionLabel.localeCompare(b.selectionLabel); };
-        if (this._orderMap !== null) {
-            orderMap = this._orderMap;
-            
-            sortValueFunction = function(a, b) {
-                if (typeof orderMap[a.selectionLabel] !== "undefined") {
-                    if (typeof orderMap[b.selectionLabel] !== "undefined") {
-                        return orderMap[a.selectionLabel] - orderMap[b.selectionLabel];
-                    } else {
-                        return -1;
-                    }
-                } else if (typeof orderMap[b.selectionLabel] !== "undefined") {
-                    return 1;
-                } else {
-                    return a.selectionLabel.localeCompare(b.selectionLabel);
-                }
-            };
-        } else if (valueType === "number") {
-            sortValueFunction = function(a, b) {
-                a = parseFloat(a.value);
-                b = parseFloat(b.value);
-                return a < b ? -1 : a > b ? 1 : 0;
-            };
-        }
-        
-        sortFunction = sortValueFunction;
-        if (this._settings.sortMode === "count") {
-            sortFunction = function(a, b) {
-                var c = b.count - a.count;
-                return c !== 0 ? c : sortValueFunction(a, b);
-            };
-        }
 
-        sortDirectionFunction = sortFunction;
-        if (this._settings.sortDirection === "reverse"){
-            sortDirectionFunction = function(a, b) {
-                return sortFunction(b, a);
-            };
-        }
-
-        entries.sort(sortDirectionFunction);
+        entries.sort(this._createSortFunction(valueType));
     }
     
     if (this._settings.showMissing || this._selectMissing) {
@@ -379,13 +367,6 @@ Exhibit.CloudFacet.prototype._computeFacet = function(items) {
     }
     
     return entries;
-};
-
-/**
- *
- */
-Exhibit.CloudFacet.prototype._notifyCollection = function() {
-    this.getUIContext().getCollection().onFacetUpdated(this);
 };
 
 /**
@@ -472,82 +453,6 @@ Exhibit.CloudFacet.prototype._constructBody = function(entries) {
     }
 };
 
-/**
- * @param {String} value
- * @param {String} label
- * @param {Boolean} selectOnly
- */
-Exhibit.CloudFacet.prototype._filter = function(value, label, selectOnly) {
-    var self, selected, select, deselect, oldValues, oldSelectMissing, newValues, newSelectMissing, actionLabel, wasSelected, wasOnlyThingSelected, newRestrictions, facetLabel;
-    self = this;
-    
-    oldValues = new Exhibit.Set(this._valueSet);
-    oldSelectMissing = this._selectMissing;
-    
-    if (typeof value === "undefined" || value === null) {
-        // the (missing this field) case
-        wasSelected = oldSelectMissing;
-        wasOnlyThingSelected = wasSelected && (oldValues.size() === 0);
-        
-        if (selectOnly) {
-            if (oldValues.size() === 0) {
-                newSelectMissing = !oldSelectMissing;
-            } else {
-                newSelectMissing = true;
-            }
-            newValues = new Exhibit.Set();
-        } else {
-            newSelectMissing = !oldSelectMissing;
-            newValues = new Exhibit.Set(oldValues);
-        }
-    } else {
-        wasSelected = oldValues.contains(value);
-        wasOnlyThingSelected = wasSelected && (oldValues.size() === 1) && !oldSelectMissing;
-        
-        if (selectOnly) {
-            newSelectMissing = false;
-            newValues = new Exhibit.Set();
-            
-            if (!oldValues.contains(value)) {
-                newValues.add(value);
-            } else if (oldValues.size() > 1 || oldSelectMissing) {
-                newValues.add(value);
-            }
-        } else {
-            newSelectMissing = oldSelectMissing;
-            newValues = new Exhibit.Set(oldValues);
-            if (newValues.contains(value)) {
-                newValues.remove(value);
-            } else {
-                newValues.add(value);
-            }
-        }
-    }
-    
-    newRestrictions = { selection: newValues.toArray(), selectMissing: newSelectMissing };
-    
-    facetLabel = this.getLabel();
-    Exhibit.History.pushComponentState(
-        this,
-        Exhibit.Facet.getRegistryKey(),
-        newRestrictions,
-        (selectOnly && !wasOnlyThingSelected) ?
-            Exhibit._("%facets.facetSelectOnlyActionTitle", label, facetLabel) :
-            Exhibit._(wasSelected ? "%facets.facetUnselectActionTitle" : "%facets.facetSelectActionTitle", label, facetLabel),
-        true
-    );
-};
-
-Exhibit.CloudFacet.prototype._clearSelections = function() {
-    Exhibit.History.pushComponentState(
-        this,
-        Exhibit.Facet.getRegistryKey(),
-        this.exportEmptyState(),
-        Exhibit._("%facets.facetClearSelectionsActionTitle", this.getLabel()),
-        true
-    );
-};
-
 Exhibit.CloudFacet.prototype._buildMaps = function() {
     var itemToValue, valueToItem, missingItems, valueType, insert, expression, database;
 
@@ -587,82 +492,4 @@ Exhibit.CloudFacet.prototype._buildMaps = function() {
         this._missingItems = missingItems;
         this._valueType = valueType;
     }
-};
-
-/**
- * @returns {Object}
- */
-Exhibit.CloudFacet.prototype.exportState = function() {
-    return this._exportState(false);
-};
-
-/**
- * @returns {Object}
- */
-Exhibit.CloudFacet.prototype.exportEmptyState = function() {
-    return this._exportState(true);
-};
-
-/**
- * @private
- * @param {Boolean} empty
- */
-Exhibit.CloudFacet.prototype._exportState = function(empty) {
-    var s = [];
-
-    if (!empty) {
-        s = this._valueSet.toArray();
-    }
-
-    return {
-        "selection": s,
-        "selectMissing": this._selectMissing
-    };
-};
-
-/**
- * @param {Object} state
- * @param {Boolean} state.selectMissing
- * @param {Array} state.selection
- */
-Exhibit.CloudFacet.prototype.importState = function(state) {
-    if (this.stateDiffers(state)) {
-        if (state.selection.length === 0 && !state.selectMissing) {
-            this.clearAllRestrictions();
-        } else {
-            this.applyRestrictions(state);
-        }
-    }
-};
-
-/**
- * Check if the state being requested for import is any different from the
- * current state.  This is only a worthwhile function to call if the check
- * is always faster than just going through with the import.
- * 
- * @param {Object} state
- * @param {Boolean} state.selectMissing
- * @param {Array} state.selection
- */
-Exhibit.CloudFacet.prototype.stateDiffers = function(state) {
-    var stateSet, stateStartCount, valueStartCount;
-
-    if (state.selectMissing !== this._selectMissing) {
-        return true;
-    }
-
-    stateStartCount = state.selection.length;
-    valueStartCount = this._valueSet.size();
-
-    if (stateStartCount !== valueStartCount) {
-        return true;
-    } else {
-        stateSet = new Exhibit.Set(state.selection);
-        stateSet.addSet(this._valueSet);
-        if (stateSet.size() !== stateStartCount) {
-            return true;
-        }
-    }
-
-    return false;
 };
