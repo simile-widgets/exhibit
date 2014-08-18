80d46542933c43b493594d139349fe3591d37b13
karger@mit.edu <karger@mit.edu>
2014-08-18 17:47:16 -0400

    Cleaned up some distracting commented-out code

--- scripted/src/scripts/ui/facets/util/debug.js
+++ scripted/src/scripts/ui/facets/util/debug.js
@@ -11,6 +11,5 @@ Timegrid.Debug.log = function(msg) {
 Timegrid.Debug.exception = function(e) {
     e = $.getIsIE() ? e.message : e;
     $.debugException(e, "Caught exception");
-    //alert("Caught exception: " + (SimileAjax.Platform.isIE ? e.message : e));
 };
 
